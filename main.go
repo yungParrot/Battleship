186a95134ed8a6016032b11fe1986a1d18f410b9
Fabian Schmidt <fabian@fabb.zone>
2023-04-19 12:29:44 +0200

    saving work in progress state

--- main.go
+++ main.go
@@ -4,11 +4,15 @@ package main
 import (
   "fmt"
   "battleship/http"
+  "battleship/battleship"
 )
 
 
 func main() {
   gameUrl := "https://go-pjatk-server.fly.dev/api/game"
-  res := http.InitGame(gameUrl)
-  fmt.Printf("Token %v\n", res)
+  authToken := http.InitGame(gameUrl)
+  fmt.Printf("Token %v\n", authToken)
+  rawPositions, _ := http.Board(gameUrl, authToken)
+  coordinates := battleship.ConvertToCoordinates(rawPositions)
+  battleship.DisplayBoard(coordinates)
 }
