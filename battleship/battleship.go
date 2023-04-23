73446fe84e007d9b54bc4dbaef399f44d797b372
Fabian Schmidt <fabian@fabb.zone>
2023-04-23 15:28:58 +0200

    saving work in progres state

--- battleship/battleship.go
+++ battleship/battleship.go
@@ -18,8 +18,7 @@ func getColumns() []string {
   return []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "J"}
 }
 
-func getRows() []string {
-  return []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
+func getRows() []string { return []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
 }
 
 
@@ -33,16 +32,30 @@ func ConvertToCoordinates(rawCoordinates []string) map[string]Coordinate {
 }
 
 
-func DisplayBoard(coordinates *map[string]Coordinate) {
+func DisplayBoard(yourCoords *map[string]Coordinate, opponentsCoords *map[string]Coordinate) {
   rows := getRows()
   columns := getColumns()
   for _, row := range rows {
-    color.Set(color.BgMagenta)
+    color.Set(color.BgBlue)
     fmt.Printf("%2s", row)
     color.Unset()
     for _, column := range columns {
       position := column + row
-      if _, found := (*coordinates)[position]; found {
+      if _, found := (*yourCoords)[position]; found {
+        fmt.Printf("%2s", "X")
+        continue
+      }
+      fmt.Printf("%2s", "-")
+    }
+
+    fmt.Printf("%2s", "\t|\t")
+
+    color.Set(color.BgRed)
+    fmt.Printf("%2s", row)
+    color.Unset()
+    for _, column := range columns {
+      position := column + row
+      if _, found := (*opponentsCoords)[position]; found {
         fmt.Printf("%2s", "X")
         continue
       }
@@ -50,11 +63,21 @@ func DisplayBoard(coordinates *map[string]Coordinate) {
     }
     fmt.Println()
   }
-  color.Set(color.BgMagenta)
+  color.Set(color.BgBlue)
   fmt.Printf("%3s", "")
   for _, column := range columns {
     fmt.Printf("%s ", column)
   }
   color.Unset()
+
+  fmt.Printf("%2s", "\t|\t")
+
+  color.Set(color.BgRed)
+  fmt.Printf("%3s", "")
+  for _, column := range columns {
+    fmt.Printf("%s ", column)
+  }
+  color.Unset()
+
   fmt.Println()
 }
--- main.go
+++ main.go
@@ -1,25 +1,54 @@
 package main
 
-
 import (
-  "fmt"
-  "battleship/http"
-  "battleship/battleship"
-  "github.com/fatih/color"
+	"battleship/battleship"
+	"battleship/http"
+	"fmt"
+	"github.com/manifoldco/promptui"
 )
 
 
+type Option struct {
+  Name string
+  Description string
+}
+
+
 func main() {
-  gameUrl := "https://go-pjatk-server.fly.dev/api/game"
-  authToken := http.InitGame(gameUrl)
-  fmt.Printf("Token %v\n", authToken)
-  rawPositions, _ := http.Board(gameUrl, authToken)
-  coordinates := battleship.ConvertToCoordinates(rawPositions)
+  for {
+    mainPrompt := promptui.Select{
+      Label: "Select an option",
+      Items: []string{
+        "h",
+        "n",
+        "q",
+      },
+    }
+    _, option, err := mainPrompt.Run()
+    if err != nil {
+      fmt.Printf("Prompt failed %v\n", err)
+      return
+    }
 
-  color.Blue("\nYour board")
-  battleship.DisplayBoard(&coordinates)
-
-  color.Red("\nYour oponent's board")
-  enemyBoard := make(map[string]battleship.Coordinate)
-  battleship.DisplayBoard(&enemyBoard)
+    switch option {
+    case "h":
+      fmt.Println("Help:")
+      fmt.Println("\th - display this help")
+      fmt.Println("\tn - create a new game")
+      fmt.Println("\tq - quit the app")
+    case "n":
+      gameUrl := "https://go-pjatk-server.fly.dev/api/game"
+      authToken := http.InitGame(gameUrl)
+      fmt.Printf("Token %v\n", authToken)
+      rawPositions, _ := http.Board(gameUrl, authToken)
+      yourCoords := battleship.ConvertToCoordinates(rawPositions)
+      opponentsCoords := make(map[string]battleship.Coordinate)
+      battleship.DisplayBoard(&yourCoords, &opponentsCoords)
+    case "q":
+      fmt.Println("Leaving the game...")
+      return
+    default:
+      fmt.Println("Unknown option %s", option)
+    }
+  }
 }
